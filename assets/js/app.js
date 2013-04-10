3a9fcfc7abe6cb7028db1d64b212eb3272846f54
Maxime Aubaret <maxime@particleslab.com>
2013-04-10 17:39:42 +0200

    Add Candlestick graph in graph.html

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+/**
+ * StoQuEx
+ * (as seen by maubaret & atoni)
+ *
+ * @author Maxime Aubaret <maxime@particleslab.com>
+ * @author Alexandre Toni <alexandre@particleslab.com>
+ */
+
+var app = (function () {
+
+})();
+
