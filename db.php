e690d7fe47d683411fa5fd939d8d5260feaa5293
Maxime Aubaret <maxime@particleslab.com>
2013-04-22 14:20:43 +0200

    Fixed db.php

--- db.php
+++ db.php
@@ -3,17 +3,16 @@ $database['dsn']        = "mysql:host=localhost;dbname=pg217;charset=utf8";
 $database['user']       = "root";
 $database['password']   = "root";
 
-if (isset($_ENV["DATABASE_DSN"]))
-  $database['server'] = $_ENV["DATABASE_DSN"];
-
-if (isset($_ENV["DATABASE_USER"]))
-  $database['server'] = $_ENV["DATABASE_USER"];
-
-if (isset($_ENV["DATABASE_PASSWORD"]))
-  $database['server'] = $_ENV["DATABASE_PASSWORD"];
+if (isset($_ENV["DATABASE_DSN"])) {
+  $database['dsn'] = $_ENV["DATABASE_DSN"];
+}
 
 try {
-  $db = new PDO($database['dsn'], $database['user'], $database['password']);
+  if (isset($_ENV["DATABASE_DSN"]))
+    $db = new PDO($_ENV["DATABASE_DSN"]);
+
+  else
+    $db = new PDO($database['dsn'], $database['user'], $database['password']);
 }
 catch (PDOException $e) {
   die ("Unable to access DB");
